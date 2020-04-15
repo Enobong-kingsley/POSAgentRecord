45a6f978fe234e2e41d59ba6ceafa9f2fc592d97
Akanimo Ekong <akanimoekong@gmail.com>
2020-04-15 15:31:50 +0100

    Updated

---
+++ app/src/main/java/com/chuks/posagentrecord/Afterlanding.kt
@@ -0,0 +1,27 @@
+package com.chuks.posagentrecord
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+import com.chuks.posagentrecord.databinding.FragmentAfterlandingBinding
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class Afterlanding : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_afterlanding, container, false)
+    }
+
+
+
+}
---
+++ app/src/main/java/com/chuks/posagentrecord/SplashScreen.kt
@@ -0,0 +1,20 @@
+package com.chuks.posagentrecord
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val handler = Handler()
+        handler.postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 2000)
+    }
+}
