1c4c10a5188b7c7ecc2159bae493de1afc1c4b5c
Akanimo Ekong <akanimoekong@gmail.com>
2020-07-28 09:18:08 +0100

    I have made some changes

--- app/src/main/java/com/chuks/posagentrecord/AddTransactionFragment.kt
+++ app/src/main/java/com/chuks/posagentrecord/AddTransactionFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
 
 /**
  * A simple [Fragment] subclass.
--- app/src/main/java/com/chuks/posagentrecord/Afterlanding.kt
+++ app/src/main/java/com/chuks/posagentrecord/Afterlanding.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.databinding.DataBindingUtil.setContentView
 
 
@@ -15,22 +16,23 @@ import com.chuks.posagentrecord.databinding.FragmentAfterlandingBinding
  * A simple [Fragment] subclass.
  */
 class Afterlanding : Fragment() {
+
     private lateinit var binding: FragmentAfterlandingBinding
 
-//    override fun onCreateView(
-//
-//        inflater: LayoutInflater, container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//        return inflater.inflate(R.layout.fragment_afterlanding, container, false)
-//    }
-
-    override fun onCreateView(savedInstanceState: Bundle?) {
-        title = "DashBoard"
-        super.onCreate(savedInstanceState)
-        binding = FragmentAfterlandingBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_afterlanding, container, false)
+        return binding.root
     }
+
+//    override fun onCreateView(savedInstanceState: Bundle?) {
+//        title = "DashBoard"
+//        super.onCreate(savedInstanceState)
+//        binding = FragmentAfterlandingBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//    }
 
 
 }
